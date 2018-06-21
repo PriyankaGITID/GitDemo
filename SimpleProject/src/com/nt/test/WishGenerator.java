830019025c7dd0c52db7ff2bb5811d2ad1edafba
Priyanka <Priyanka@DESKTOP-KP7OFB7>
2018-06-21 18:07:57 +0530

    tr1

--- SimpleProject/src/com/nt/test/WishGenerator.java
+++ SimpleProject/src/com/nt/test/WishGenerator.java
@@ -5,6 +5,8 @@ public class WishGenerator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(System.nanoTime());
+		
+		System.out.println("hhhhhhhhh");
 
 	}
 
